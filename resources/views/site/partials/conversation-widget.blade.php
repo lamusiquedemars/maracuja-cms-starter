9bc88646ab4c057158a30d7b352b2fa2c58e1568
lamusiquedemars <ivocore@gmail.com>
2026-07-28 13:03:09 +0200

    Give conversation history more space

--- resources/views/site/partials/conversation-widget.blade.php
+++ resources/views/site/partials/conversation-widget.blade.php
@@ -32,9 +32,10 @@
             <button type="button" aria-label="Fermer la conversation" data-conversation-close>×</button>
         </header>
 
-        <div class="conversation-widget__notice">
-            {{ $conversationSettings->privacy_notice }}
-        </div>
+        <details class="conversation-widget__notice">
+            <summary>À propos de cet échange</summary>
+            <p>{{ $conversationSettings->privacy_notice }}</p>
+        </details>
 
         <div
             class="conversation-widget__messages"
@@ -61,7 +62,7 @@
                 id="conversation-message"
                 name="content"
                 maxlength="5000"
-                rows="3"
+                rows="2"
                 required
                 data-conversation-input
             ></textarea>
